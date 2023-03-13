7385431cf03c7b9f673fda61ec5471c242f5ede5
KishorKR <76257893+kishor-17@users.noreply.github.com>
2023-03-13 11:20:03 +0530

    code smell

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import './App.css';
 import React from 'react';
 import ReactDOM from "react-dom/client";
 
-import { BrowserRouter, Routes, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   return (
 
